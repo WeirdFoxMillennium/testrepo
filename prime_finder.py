5442f896bcaea4c56a0d14d31c51a12653a90637
Константин Сельянов <User@DESKTOP-MUIBU3E.tpsk.lan>
2024-05-29 11:43:38 +0300

    Исправление 5

--- prime_finder.py
+++ prime_finder.py
@@ -6,12 +6,8 @@ def sieve_of_eratosthenes(n):
             for i in range(p * p, n+1, p):
                 primes[i] = False
         p += 1
-
     prime_numbers = [p for p in range(2, n) if primes[p]]
     return prime_numbers
-
 n = int(input("Введите число n для поиска простых чисел: "))
 print(f"Простые числа до {n}:")
 print(sieve_of_eratosthenes(n))
-
-print()
