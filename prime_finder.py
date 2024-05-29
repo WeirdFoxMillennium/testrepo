79c19063e6ba300f1f2e6910ae6ec4134ae563f5
Константин Сельянов <User@DESKTOP-MUIBU3E.tpsk.lan>
2024-05-29 11:14:19 +0300

    Исправление стиля кода для flake8

--- prime_finder.py
+++ prime_finder.py
@@ -1,8 +1,8 @@
 def sieve_of_eratosthenes(n):
-    primes = [True for i in range(n+1)]
+    primes = [True for _ in range(n+1)]
     p = 2
-    while (p * p <= n):
-        if (primes[p] == True):
+    while p * p <= n:
+        if primes[p]:
             for i in range(p * p, n+1, p):
                 primes[i] = False
         p += 1
